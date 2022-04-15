d4e9e5596ada2c49c057cb37bcf3542588e6a1ee
wendy03 <wendysu03@gmail.com>
2022-04-15 23:40:45 +0800

    connect DB

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './.env' });
 
 const port = process.env.PORT;
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('Connect DB'))
+  .catch((err) => console.log(err));
 
 const requestListener = (req, res) => {
   console.log(req.url);
